f0e4923cd7de9374880eac80d8e65552019011d2
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2024-03-17 13:49:49 +0800

    Added onKeyEvent
    - Still not working, onKeyEvent is not triggering

--- app/src/main/java/com/example/philippinecurrencyidentifier/MyAccessibilityService.kt
+++ app/src/main/java/com/example/philippinecurrencyidentifier/MyAccessibilityService.kt
@@ -2,33 +2,51 @@ package com.example.philippinecurrencyidentifier
 
 import android.accessibilityservice.AccessibilityService
 import android.accessibilityservice.AccessibilityServiceInfo
+import android.util.Log
+import android.view.KeyEvent
 import android.view.accessibility.AccessibilityEvent
 
-class MyAccessibilityService : AccessibilityService() {
-    override fun onInterrupt() {}
 
-    override fun onAccessibilityEvent(event: AccessibilityEvent?) {}
+class MyAccessibilityService : AccessibilityService() {
+
+    override fun onKeyEvent(event: KeyEvent): Boolean {
+
+        Log.d("MyAccessibilityService", "onKeyEvent: initialized")
+
+        val action = event.action
+        val keyCode = event.keyCode
+        return if (action == KeyEvent.ACTION_UP) {
+            if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+                Log.d("MyAccessibilityService", "KeyUp")
+            } else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+                Log.d("MyAccessibilityService", "KeyDown")
+            }
+            true
+        } else {
+            super.onKeyEvent(event)
+        }
+    }
 
     override fun onServiceConnected() {
+
         val info = AccessibilityServiceInfo()
 
-        // Set the type of events that this service wants to listen to. Others
-        // aren't passed to this service.
+        // Configuration code as you have it...
         info.eventTypes = AccessibilityEvent.TYPE_VIEW_CLICKED or AccessibilityEvent.TYPE_VIEW_FOCUSED
-
-        // If you only want this service to work with specific apps, set their
-        // package names here. Otherwise, when the service is activated, it
-        // listens to events from all apps.
         info.packageNames = arrayOf("com.example.android.myFirstApp", "com.example.android.mySecondApp")
-
-        // Set the type of feedback your service provides.
         info.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN
-
-        // Here you can set additional flags, for example:
-        // info.flags = AccessibilityServiceInfo.DEFAULT
-
         info.notificationTimeout = 100
-
         this.serviceInfo = info
+
+        // TRIGGERS TO OPEN THE APP
+        // NEED TO MOVE TO A FUNCTION THAT TRIGGERS WHEN A VOLUME KEY WAS PRESSED
+//        val launchIntent = Intent(this, MainActivity::class.java)
+//        launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//        startActivity(launchIntent)
+
+        Log.d("MyAccessibilityService", "SERVICE CONNECTED")
     }
+
+    override fun onInterrupt() {}
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {}
 }
